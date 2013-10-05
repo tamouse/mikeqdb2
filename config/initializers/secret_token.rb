72cb2795063dba6750da11841d453bd5f84c8af0
Tamara Temple <tamouse@gmail.com>
2013-10-04 21:37:37 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mikeqdb2::Application.config.secret_key_base = 'd83508fcfc019f58b473695356582115a07b4705e9cf24fe5e83d497fcbe5267e8d69bebcaf809a30e9405c1330330704121bafe13368a24e01894f17a21c79c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mikeqdb2::Application.config.session_store :cookie_store, key: '_mikeqdb2_session'
